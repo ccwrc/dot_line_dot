cddb00aa8c576757958db9edd5756341b08aeb94
ccwrc <j.muszelik@gmail.com>
2017-05-16 16:48:27 +0200

    add index

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+
+<!DOCTYPE HTML>
+<html lang="pl">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+        <title> dot </title>
+        <meta name="description" content="dummy desc" />
+        <meta name="keywords" content="dummy " />
+        <meta name="author" content="ccwrc">
+        <link rel="stylesheet" href="css/style.css" type="text/css" />
+<!--        <script src="js/jquery-3.1.1.min.js"></script>
+        <script src="js/app.js"></script>	-->
+    </head>
+    <body>
+        
+        
+
+    </body>
+</html>
+
