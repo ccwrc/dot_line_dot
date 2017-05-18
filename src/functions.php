c6f6dbefdb4e58eebac8d285b2138af09e903ca7
ccwrc <j.muszelik@gmail.com>
2017-05-18 13:39:44 +0200

    function translateMorseToHuman - done

--- src/functions.php
+++ src/functions.php
@@ -2,18 +2,42 @@
 
 require_once 'translate.php';
 
+
 function translateHumanToMorse($stringToTranslate, array $morseCode) {
 
     if (!is_string($stringToTranslate) || !is_array($morseCode)) {
         return "";
     }
 
-    $stringToTranslate = trim(strtolower($stringToTranslate));
+    $lowerStringToTranslate = trim(strtolower($stringToTranslate));
     $retString = "";
-    for ($i = 0; $i <= strlen($stringToTranslate) - 1; $i++) {
-        foreach ($morseCode as $humanWord => $morseChar) {
-            if ($stringToTranslate[$i] === $humanWord) {
+    for ($i = 0; $i <= strlen($lowerStringToTranslate) - 1; $i++) {
+        foreach ($morseCode as $humanChar => $morseChar) {
+            if ($lowerStringToTranslate[$i] === $humanChar) {
                 $retString .= $morseChar;
+                $retString .= " ";
+            } else {
+                $retString .= " ";
+            }
+        }
+    }
+
+    return $retString;
+}
+
+
+function translateMorseToHuman($stringToTranslate, array $morseCode) {
+
+    if (!is_string($stringToTranslate) || !is_array($morseCode)) {
+        return "";
+    }
+
+    $arrayToTranslate = explode(" ", $stringToTranslate);
+    $retString = "";
+    for ($i = 0; $i <= count($arrayToTranslate) - 1; $i++) {
+        foreach ($morseCode as $humanChar => $morseChar) {
+            if ($arrayToTranslate[$i] === $morseChar) {
+                $retString .= $humanChar;
                 $retString .= " ";
             } else {
                 $retString .= " ";
