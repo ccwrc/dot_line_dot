d491ff4ad58e073f98087e57c30a269cf2b401f8
ccwrc <j.muszelik@gmail.com>
2017-05-27 17:38:42 +0200

    head update, add favicon

--- index.php
+++ index.php
@@ -46,6 +46,13 @@ if (!isset($_SESSION['language'])) {
         <link rel="stylesheet" href="css/style.css" type="text/css" />
         <script src="js/jquery-3.2.1.min.js"></script>
         <script src="js/app.js"></script>	
+        
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+        <link rel="manifest" href="/manifest.json">
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+        <meta name="theme-color" content="#ffffff">
     </head>
     <body>    
         <div id="divContainer">
